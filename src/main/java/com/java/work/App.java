688b23956e7606b43189c8a52238873a90a57118
Sonish Maharjan <sonish.maharjan@cloudchomp.com>
2019-07-05 13:47:31 +0545

    inital commit

---
+++ src/main/java/com/java/work/App.java
@@ -0,0 +1,13 @@
+package com.java.work;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "jalkoa World!" );
+    }
+}
---
+++ src/main/java/com/java/work/MyJava.java
@@ -0,0 +1,13 @@
+package com.java.work;
+
+/**
+ * Hello world!
+ *
+ */
+public class MyJava 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Javato  World!" );
+    }
+}
